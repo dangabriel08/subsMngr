ae8d52de4e9df5a8fe305a98468fbee01db47a1e
Daniel Gabriel <daniel@dangabriel.me>
2022-06-16 04:58:30 +0800

    small updates on subs and subs service

--- src/app/services/subs.service.ts
+++ src/app/services/subs.service.ts
@@ -2,21 +2,31 @@ import { HttpClient ,HttpHeaders} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-
+export interface subsObject {
+  id: number;
+  user_id: number;
+  subs_name: string;
+  billing_date: string;
+  payment_method_used: string;
+  payment_method_type: string;
+  created_at: Date;
+  updated_at: Date;
+}
 @Injectable({
   providedIn: 'root'
 })
+
 export class SubsService {
 
   constructor(private http: HttpClient) { }
 
-  getSubs():Observable<any>{
+  getSubs():Observable<subsObject>{
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${environment.tempAccessToken}`
     });
     
   const requestOptions = { headers: headers };
-    return this.http.get(`${environment.getSubsEndpoint}3`,requestOptions)
+    return this.http.get<subsObject>(`${environment.getSubsEndpoint}3`,requestOptions)
   }
 }
--- src/app/subs/subs.page.ts
+++ src/app/subs/subs.page.ts
@@ -9,12 +9,14 @@ import { SubsService } from '../services/subs.service';
 export class SubsPage implements OnInit {
 
   constructor(private subsService:SubsService) { }
-
+  subs  = <any>[];
   ngOnInit() {
    this.getSubs();
   }
+
    getSubs() {
     this.subsService.getSubs().subscribe(res=>{
+      this.subs = res;
       console.log(res);
     });
   }
