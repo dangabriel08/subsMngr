9f77d9f550731b2db0fc026d4b817bdb67f2b5cc
Daniel Gabriel <daniel@dangabriel.me>
2022-06-23 19:41:45 +0800

    added ios platform

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.danagbriel.subsmngr',
+  appId: 'io.dangabriel.subsmngr',
   appName: 'subsMngr',
   webDir: 'www',
   bundledWebRuntime: false
